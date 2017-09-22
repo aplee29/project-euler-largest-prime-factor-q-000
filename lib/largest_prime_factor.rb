9960064d4b92dba6f2682e6d1ace38c3fcd517b9
Ann Lee <aplee29@users.noreply.github.com>
2017-09-22 01:27:06 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,11 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+  prime = input
+  (2..Math.sqrt(input).to_i).each do |i|
+    # Initiates while loop to reduce prime value by i repeatedly, starting from 2
+    # i increments by 1 until value is no longer evenly divisible
+    while prime > i && prime % i == 0
+      prime /= i
+    end
+  end
+  prime
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_reader :number
+
+  def initialize(input)
+    @number = largest_prime_factor(input)
+  end
+
+  def largest_prime_factor(input)
+    prime = input
+    (2..Math.sqrt(input).to_i).each do |i|
+      while prime > i && prime % i == 0
+        prime /= i
+      end
+    end
+    prime
+  end
+end
